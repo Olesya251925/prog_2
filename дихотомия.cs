dc2d9ff27dec73cf1189082215e42c06bfccd1c6
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-10-23 13:38:10 +0700

    Update дихотомия.cs

--- дихотомия.cs
+++ дихотомия.cs
@@ -36,69 +36,84 @@ namespace OptimizationApp
         public MainForm()
         {
             InitializeComponents();
+            this.Size = new System.Drawing.Size(900, 700);
         }
 
         private void InitializeComponents()
         {
-            // Создание текстовых полей
+            // Создаем основной контейнер
+            Panel mainPanel = new Panel();
+            mainPanel.Dock = DockStyle.Fill;
+            this.Controls.Add(mainPanel);
+
+            // Введите значения a, b, e
             labelA = new Label();
             labelA.Text = "Введите значение a:";
             labelA.Location = new System.Drawing.Point(10, 10);
             labelA.AutoSize = true;
-            this.Controls.Add(labelA);
+            mainPanel.Controls.Add(labelA);
 
             textBoxA = new TextBox();
             textBoxA.Location = new System.Drawing.Point(labelA.Right + 5, 10);
             textBoxA.Size = new System.Drawing.Size(100, 20);
-            this.Controls.Add(textBoxA);
+            mainPanel.Controls.Add(textBoxA);
 
             labelB = new Label();
             labelB.Text = "Введите значение b:";
             labelB.Location = new System.Drawing.Point(10, 40);
             labelB.AutoSize = true;
-            this.Controls.Add(labelB);
+            mainPanel.Controls.Add(labelB);
 
             textBoxB = new TextBox();
             textBoxB.Location = new System.Drawing.Point(labelB.Right + 5, 40);
             textBoxB.Size = new System.Drawing.Size(100, 20);
-            this.Controls.Add(textBoxB);
+            mainPanel.Controls.Add(textBoxB);
 
             labelE = new Label();
             labelE.Text = "Введите значение точности e:";
             labelE.Location = new System.Drawing.Point(10, 70);
             labelE.AutoSize = true;
-            this.Controls.Add(labelE);
+            mainPanel.Controls.Add(labelE);
 
             textBoxE = new TextBox();
             textBoxE.Location = new System.Drawing.Point(labelE.Right + 5, 70);
             textBoxE.Size = new System.Drawing.Size(100, 20);
-            this.Controls.Add(textBoxE);
+            mainPanel.Controls.Add(textBoxE);
 
-            // Создание кнопки "Рассчитать"
+            // Кнопки Рассчитать и Очистить
             calculateButton = new Button();
             calculateButton.Text = "Рассчитать";
             calculateButton.Location = new System.Drawing.Point(10, 100);
             calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
-            this.Controls.Add(calculateButton);
+            mainPanel.Controls.Add(calculateButton);
 
-            // Создание кнопки "Очистить"
             clearButton = new Button();
             clearButton.Text = "Очистить";
             clearButton.Location = new System.Drawing.Point(calculateButton.Left + calculateButton.Width + 10, 100);
             clearButton.Click += new System.EventHandler(this.ClearButton_Click);
-            this.Controls.Add(clearButton);
+            mainPanel.Controls.Add(clearButton);
 
+            // Текстовое поле для вывода результатов
             localPointsTextBox = new TextBox();
             localPointsTextBox.Multiline = true;
             localPointsTextBox.ScrollBars = ScrollBars.Vertical;
-            localPointsTextBox.Location = new System.Drawing.Point(10, 190);
+            localPointsTextBox.Location = new System.Drawing.Point(10, 130);
             localPointsTextBox.Size = new System.Drawing.Size(300, 60);
-            this.Controls.Add(localPointsTextBox);
+            mainPanel.Controls.Add(localPointsTextBox);
 
-            // Создание PlotView для отображения графика с точками
+            // Высота под график 
+            int graphHeight = 500;
+
+            // Создаем контейнер для графика
+            Panel graphContainer = new Panel();
+            graphContainer.Dock = DockStyle.Bottom; // Док-стиль изменен на Bottom, чтобы занимать нижнюю часть основного контейнера
+            graphContainer.Height = graphHeight; // Устанавливаем высоту контейнера
+            mainPanel.Controls.Add(graphContainer);
+
+            // Создаем и добавляем PlotView
             plotView = new PlotView();
-            plotView.Dock = DockStyle.Fill;
-            this.Controls.Add(plotView);
+            plotView.Dock = DockStyle.Fill; // Делаем его заполняющим для родительского контейнера
+            graphContainer.Controls.Add(plotView);
         }
 
         private void CalculateButton_Click(object sender, EventArgs e)
@@ -140,15 +155,37 @@ namespace OptimizationApp
 
         private void ShowGraphWithRootPoint(double a, double b, Func<double, double> function, double rootResult, double epsilon)
         {
-            // Увеличиваем интервал для построения графика
-            double expandedA = a - 1;
-            double expandedB = b + 1;
+            // Создание модели графика
+            var model = new PlotModel { Title = "График функции" };
 
-            // Создание новой формы с графиком
-            using (GraphForm graphForm = new GraphForm(expandedA, expandedB, function, rootResult, epsilon))
+            // Добавление сетки
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Bottom, MajorGridlineStyle = LineStyle.Solid, MajorGridlineColor = OxyColors.LightGray });
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Solid, MajorGridlineColor = OxyColors.LightGray });
+
+            var lineSeries = new LineSeries();
+            int numberOfPoints = (int)((b - a) / epsilon);
+
+            for (int i = 0; i <= numberOfPoints; i++)
             {
-                graphForm.ShowDialog();
+                double x = a + i * epsilon;
+                lineSeries.Points.Add(new DataPoint(x, function(x)));
             }
+
+            model.Series.Add(lineSeries);
+
+            // Добавление точки пересечения с осью X на график
+            var rootAnnotation = new OxyPlot.Annotations.PointAnnotation
+            {
+                X = rootResult,
+                Y = 0,
+                Text = "Root",
+                TextPosition = new DataPoint(rootResult, 1),
+                TextColor = OxyColors.Green
+            };
+            model.Annotations.Add(rootAnnotation);
+
+            // Установка модели для существующего PlotView
+            plotView.Model = model;
         }
 
         private void ClearButton_Click(object sender, EventArgs e)
@@ -218,58 +255,6 @@ namespace OptimizationApp
             string epsilonString = number.ToString(System.Globalization.CultureInfo.InvariantCulture);
             int decimalPlaces = epsilonString.Length - epsilonString.IndexOf('.') - 1;
             return decimalPlaces;
-        }
-    }
-
-    public class GraphForm : Form
-    {
-        private PlotView plotView;
-
-        public GraphForm(double a, double b, Func<double, double> function, double rootResult, double epsilon)
-        {
-            InitializeComponents(a, b, function, rootResult, epsilon);
-        }
-
-        private void InitializeComponents(double a, double b, Func<double, double> function, double rootResult, double epsilon)
-        {
-            double expandedA = a - 1;
-            double expandedB = b + 1;
-
-            plotView = new PlotView();
-            plotView.Dock = DockStyle.Fill;
-            this.Controls.Add(plotView);
-
-            // Создание модели графика
-            var model = new PlotModel { Title = "График функции" };
-
-            // Добавление сетки
-            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Bottom, MajorGridlineStyle = LineStyle.Solid, MajorGridlineColor = OxyColors.LightGray });
-            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Solid, MajorGridlineColor = OxyColors.LightGray });
-
-            var lineSeries = new LineSeries();
-            int numberOfPoints = (int)((expandedB - expandedA) / epsilon);
-
-            for (int i = 0; i <= numberOfPoints; i++)
-            {
-                double x = expandedA + i * epsilon;
-                lineSeries.Points.Add(new DataPoint(x, function(x)));
-            }
-
-            model.Series.Add(lineSeries);
-
-            // Добавление точки пересечения с осью X на график
-            var rootAnnotation = new OxyPlot.Annotations.PointAnnotation
-            {
-                X = rootResult,
-                Y = 0,
-                Text = "Root",
-                TextPosition = new DataPoint(rootResult, 1),
-                TextColor = OxyColors.Green
-            };
-            model.Annotations.Add(rootAnnotation);
-
-            // Установка модели для PlotView
-            plotView.Model = model;
         }
     }
 }
